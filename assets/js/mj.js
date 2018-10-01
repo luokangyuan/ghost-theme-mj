9de62d0c2b5a008b2e8739e37b546e1fac7174ee
luokangyuan <243518985@qq.com>
2018-10-02 02:05:41 +0800

    添加知识技能图和个人能力进步图

---
+++ assets/js/mj.js
@@ -0,0 +1,6 @@
+$(function(){
+    var color = ["#FFCC00","#4CD964","#34AADC","#007AFF","#5856D6","#FF2D55","#1D77EF","#FF3A2D","#5BCAFF","#007AFF","#5856D6","#FF2D55","#1D77EF","#FF3A2D","#5BCAFF","#FFCC00","#4CD964","#34AADC",];
+   $(".classificationColor").each(function(index,item){
+       $(item).css("background-color",color[index])
+   })
+})
