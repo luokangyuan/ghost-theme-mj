c7aa8377f8fcb8e03cfd56e99d102b50dde1342b
luokangyuan <243518985@qq.com>
2018-10-15 22:35:49 +0800

    新增文章打赏功能

--- assets/js/mj.js
+++ assets/js/mj.js
@@ -1,7 +1,8 @@
 $(function(){
     var color = ["#FFCC00","#4CD964","#34AADC","#007AFF","#5856D6","#FF2D55","#1D77EF","#FF3A2D","#5BCAFF","#007AFF","#5856D6","#FF2D55","#1D77EF","#FF3A2D","#5BCAFF","#FFCC00","#4CD964","#34AADC",];
    $(".classificationColor").each(function(index,item){
-       $(item).css("background-color",color[index])
+       $(item).css("background-color",color[index%color.length])
    })
 })
+
 
