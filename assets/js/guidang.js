3c72107cd2cd21b1382ded17d6f383bcfb48f1fd
luokangyuan <243518985@qq.com>
2018-11-23 22:29:43 +0800

    修改文章数大于4才显示技能标签

--- assets/js/guidang.js
+++ assets/js/guidang.js
@@ -8,7 +8,7 @@ $.get(ghost.url.api('tags', {limit: 'all', 'include': 'count.posts'})).done(func
     var indicatorArrTmp = [];
     var max = 0;
     for(var tagItem in data.tags){
-        if(data.tags[tagItem].count.posts > 1){
+        if(data.tags[tagItem].count.posts > 4){
             var indicatorTmp = {}
             indicatorTmp.name = data.tags[tagItem].name;
             indicatorTmp.max = 50;
