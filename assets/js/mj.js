000a33eb5c43dbcd450c1a34ad7f230e9385378c
luokangyuan <243518985@qq.com>
2018-11-24 23:35:54 +0800

    x修改词云图显示所有标签

--- assets/js/mj.js
+++ assets/js/mj.js
@@ -3,13 +3,31 @@ $(function () {
     $(".classificationColor").each(function (index, item) {
         $(item).css("background-color", color[index % color.length])
     })
-    $(".cd-timeline-content").each(function(index,item){
-        var frind = "frind-card" + ((index % 10)+1);
+    $(".cd-timeline-content").each(function (index, item) {
+        var frind = "frind-card" + ((index % 10) + 1);
         $(item).addClass(frind);
     })
 
+    var word_list = [];
+    var obj;
+    $.get(ghost.url.api('tags', { include: "count.posts" ,limit: "all"})).done(function (data) {
+        $.each(data.tags, function (index, item) {
+            obj = new Object();
+            obj.text = item.name;
+            obj.weight = item.count.posts;
+            obj.link = "/articles/classification/" + item.slug
+            word_list.push(obj);
+        })
+        $("#tag-wordcloud").jQCloud(word_list, {
+           
+        });
+    }).fail(function (err) {
+        console.log(err);
+    });
     
-    
+
+
+
 })
 
 
